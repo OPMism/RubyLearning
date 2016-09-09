bd17d1d324b7c6deab280bbda544c22296bf40e0
OPMism <omprakashcpt@yahoo.in>
2016-09-09 01:02:26 -0500

    Added programs

---
+++ input.rb
@@ -0,0 +1,8 @@
+# Program gets user input & prints the output in console
+puts "Welcome to 'Get My Number!'"
+print "What's your name? "
+input = gets
+puts "Welcome, #{input}!"
+
+#Printing in multiple lines
+puts "First line", "Second line", "Third line"
